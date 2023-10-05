090aab316b5c8a84ea7f5dcad6a540a3d60990c9
Vladislav Gernet <skeimorr@gmail.com>
2023-10-05 19:04:21 +0400

    a solution

--- js/navigation-controls.js
+++ js/navigation-controls.js
@@ -2,16 +2,20 @@ import {navigationControls} from './variables.js';
 import {renderTable} from './render-table.js';
 import {filterValues} from './constants.js';
 import {receivedData} from './main.js';
+import {debounce} from './util.js';
 
 const onNavigationButtonClick = (evt) => {
   const selectedButton = evt.target.closest('.tabs__control');
   if (selectedButton !== null) {
     const isSelectedButtonNotActivated = selectedButton.classList.contains('is-active') === false;
     if (isSelectedButtonNotActivated) {
-      renderTable(receivedData, filterValues[selectedButton.textContent]);
-      const activeButton = navigationControls.querySelector('.is-active');
-      activeButton.classList.remove('is-active');
-      selectedButton.classList.add('is-active');
+      const debounced = debounce(() => {
+        renderTable(receivedData, filterValues[selectedButton.textContent]);
+        const activeButton = navigationControls.querySelector('.is-active');
+        activeButton.classList.remove('is-active');
+        selectedButton.classList.add('is-active');
+      });
+      debounced();
     }
   }
 };
