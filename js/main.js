4c5a7509c2839b7b3bb2fc53177e35cffbd4822f
Vladislav Gernet <skeimorr@gmail.com>
2023-10-05 21:18:15 +0400

    map dev is started

--- js/main.js
+++ js/main.js
@@ -26,4 +26,56 @@ getContractors().
     checkedUsersInput.addEventListener('change', onCheckedUsersInputChange);
   });
 
+
+//MAP
+
+//Temporary values
+const usersList = document.querySelector('.users-list');
+const mapContainer = document.querySelector('#map').closest('.container');
+usersList.style.display = 'none';
+mapContainer.style.display = 'block';
+
+//Constants
+const ZOOM = 13;
+const TILE_LAYER = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const COPYRIGHT = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const startCoordinates = {
+  lat: 59.92749,
+  lng: 30.31127,
+};
+const iconConfig = {
+  default: {
+    url: './img/pin.svg',
+    width: 36,
+    height: 46,
+    anchorX: 18,
+    anchorY: 46,
+  },
+  verified: {
+    url: './img/pin-verified.svg',
+    width: 36,
+    height: 46,
+    anchorX: 18,
+    anchorY: 46,
+  }
+};
+const pinIcon = L.icon({
+  iconUrl: iconConfig.default.url,
+  iconSize: [iconConfig.default.width, iconConfig.default.height],
+  iconAnchor: [iconConfig.default.anchorX, iconConfig.default.anchorY],
+});
+const verifiedPinIcon = L.icon({
+  iconUrl: iconConfig.verified.url,
+  iconSize: [iconConfig.verified.width, iconConfig.verified.height],
+  iconAnchor: [iconConfig.verified.anchorX, iconConfig.verified.anchorY],
+});
+//Main code
+
+
+const map = L.map('map').setView([startCoordinates.lat, startCoordinates.lng], ZOOM);
+
+L.tileLayer(TILE_LAYER, {
+  attribution: COPYRIGHT
+}).addTo(map);
+
 export {receivedData};
