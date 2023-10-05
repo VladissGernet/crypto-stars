d1d46d18c6d0100d75254746490a5d925bcc6d7b
Vladislav Gernet <skeimorr@gmail.com>
2023-10-05 16:30:27 +0400

    debounce added

--- js/main.js
+++ js/main.js
@@ -2,17 +2,19 @@ import {getContractors} from './load-data.js';
 import {navigationControls} from './variables.js';
 import {renderTable} from './render-table.js';
 import {onNavigationButtonClick} from './navigation-controls.js';
+import {debounce} from './util.js';
 
 //Не забудь добавить debounce
 
 //Если выбрано "Купить"
+const debouncedOnNavigationButtonClick = debounce(onNavigationButtonClick);
 
 let receivedData = [];
 getContractors().
   then((data) => {
     receivedData = data.slice();
     renderTable(data);
-    navigationControls.addEventListener('click', onNavigationButtonClick);
+    navigationControls.addEventListener('click', debouncedOnNavigationButtonClick);
   });
 
 export {receivedData};
