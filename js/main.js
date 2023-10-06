8630bdb6c4c9cae77b9636ce0c9a24bdd0c13f23
Vladislav Gernet <skeimorr@gmail.com>
2023-10-06 14:55:13 +0400

    show only verified added

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 import {getContractors} from './load-data.js';
 import {
   buySellContainer,
-  checkedUsersCheckbox,
+  checkedUsersCheckbox, mapContainer,
   toggleListMapContainer,
 } from './variables.js';
 import {renderTable} from './render-table.js';
 import {onNavigationButtonClick, onToggleListMapContainerClick} from './navigation-controls.js';
 import {debounce} from './util.js';
-import {initMap} from './map.js';
-import {filterValues} from './constants.js';
+import {COPYRIGHT, filterValues, pinIcon, startCoordinates, TILE_LAYER, verifiedPinIcon, ZOOM} from './constants.js';
+import {createMarker} from './map-marker-creator.js';
 
 let receivedData = [];
 getContractors().
@@ -19,18 +19,45 @@ getContractors().
       true: receivedData.filter((element) => element.isVerified === true),
       false: data,
     };
+    mapContainer.style.display = 'block'; //Необходимо для правильного отображения краты.
+    mapContainer.classList.add('visually-hidden');
+    const map = L.map('map').setView([startCoordinates.lat, startCoordinates.lng], ZOOM);
+    L.tileLayer(TILE_LAYER, {
+      attribution: COPYRIGHT
+    }).addTo(map);
+    const defaultMarkerGroup = L.layerGroup().addTo(map);
+    const verifiedMarkerGroup = L.layerGroup().addTo(map);
+    const createMarkers = (markersData, defaultMarkers, verifiedMarkers) => {
+      markersData.filter((dataElement) => {
+        if (dataElement.coords !== undefined) {
+          if (dataElement.isVerified) {
+            createMarker(dataElement.coords, verifiedPinIcon, verifiedMarkers);
+          } else {
+            createMarker(dataElement.coords, pinIcon, defaultMarkers);
+          }
+          return true;
+        }
+      });
+    };
+    createMarkers(receivedData, defaultMarkerGroup, verifiedMarkerGroup);
+
     const onCheckedUsersInputChange = () => {
       const debouncedChange = debounce(() => {
         const activeTabButton = buySellContainer.querySelector('.is-active');
         receivedData = checkedUsersInputValues[checkedUsersCheckbox.checked];
         renderTable(receivedData, filterValues[activeTabButton.textContent]);
+        if (checkedUsersCheckbox.checked === true) {
+          defaultMarkerGroup.removeFrom(map);
+        }
+        if (checkedUsersCheckbox.checked === false) {
+          defaultMarkerGroup.addTo(map);
+        }
       });
       debouncedChange();
     };
     buySellContainer.addEventListener('click', onNavigationButtonClick);
     checkedUsersCheckbox.addEventListener('change', onCheckedUsersInputChange);
     toggleListMapContainer.addEventListener('click', onToggleListMapContainerClick);
-    initMap(receivedData);
   });
 
 export {receivedData};
