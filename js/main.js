b9dd451bfd319075ac621bc4fdfaac561e6f1e94
Vladislav Gernet <skeimorr@gmail.com>
2023-10-06 09:02:48 +0400

    pins are added

--- js/main.js
+++ js/main.js
@@ -24,58 +24,83 @@ getContractors().
     };
     navigationControls.addEventListener('click', onNavigationButtonClick);
     checkedUsersInput.addEventListener('change', onCheckedUsersInputChange);
+
+
+    //MAP
+
+    //Temporary values
+    const usersList = document.querySelector('.users-list');
+    const mapContainer = document.querySelector('#map').closest('.container');
+    usersList.style.display = 'none';
+    mapContainer.style.display = 'block';
+
+    //Constants
+    const ZOOM = 10;
+    const TILE_LAYER = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const COPYRIGHT = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    const startCoordinates = {
+      lat: 59.92749,
+      lng: 30.31127,
+    };
+    const iconConfig = {
+      default: {
+        url: './img/pin.svg',
+        width: 36,
+        height: 46,
+        anchorX: 18,
+        anchorY: 46,
+      },
+      verified: {
+        url: './img/pin-verified.svg',
+        width: 36,
+        height: 46,
+        anchorX: 18,
+        anchorY: 46,
+      }
+    };
+    const pinIcon = L.icon({
+      iconUrl: iconConfig.default.url,
+      iconSize: [iconConfig.default.width, iconConfig.default.height],
+      iconAnchor: [iconConfig.default.anchorX, iconConfig.default.anchorY],
+    });
+    const verifiedPinIcon = L.icon({
+      iconUrl: iconConfig.verified.url,
+      iconSize: [iconConfig.verified.width, iconConfig.verified.height],
+      iconAnchor: [iconConfig.verified.anchorX, iconConfig.verified.anchorY],
+    });
+    //Main code
+
+
+    const map = L.map('map').setView([startCoordinates.lat, startCoordinates.lng], ZOOM);
+
+    L.tileLayer(TILE_LAYER, {
+      attribution: COPYRIGHT
+    }).addTo(map);
+
+    const markerGroup = L.layerGroup().addTo(map);
+  console.log(pinIcon)
+    const createMarkers = (markersData, markers) => {
+      markersData.filter((dataElement) => {
+        if (dataElement.coords !== undefined) {
+          const {lat, lng} = dataElement.coords;
+          const marker = L.marker(
+            {
+              lat,
+              lng,
+            },
+            {
+              pinIcon
+            },
+          );
+          marker
+            .addTo(markers);
+          return true;
+        }
+      });
+    };
+
+    createMarkers(receivedData, markerGroup);
   });
 
-
-//MAP
-
-//Temporary values
-const usersList = document.querySelector('.users-list');
-const mapContainer = document.querySelector('#map').closest('.container');
-usersList.style.display = 'none';
-mapContainer.style.display = 'block';
-
-//Constants
-const ZOOM = 13;
-const TILE_LAYER = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const COPYRIGHT = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-const startCoordinates = {
-  lat: 59.92749,
-  lng: 30.31127,
-};
-const iconConfig = {
-  default: {
-    url: './img/pin.svg',
-    width: 36,
-    height: 46,
-    anchorX: 18,
-    anchorY: 46,
-  },
-  verified: {
-    url: './img/pin-verified.svg',
-    width: 36,
-    height: 46,
-    anchorX: 18,
-    anchorY: 46,
-  }
-};
-const pinIcon = L.icon({
-  iconUrl: iconConfig.default.url,
-  iconSize: [iconConfig.default.width, iconConfig.default.height],
-  iconAnchor: [iconConfig.default.anchorX, iconConfig.default.anchorY],
-});
-const verifiedPinIcon = L.icon({
-  iconUrl: iconConfig.verified.url,
-  iconSize: [iconConfig.verified.width, iconConfig.verified.height],
-  iconAnchor: [iconConfig.verified.anchorX, iconConfig.verified.anchorY],
-});
-//Main code
-
-
-const map = L.map('map').setView([startCoordinates.lat, startCoordinates.lng], ZOOM);
-
-L.tileLayer(TILE_LAYER, {
-  attribution: COPYRIGHT
-}).addTo(map);
 
 export {receivedData};
