5f607f3faa4673760d9120d05b86cf672ecdc174
Vladislav Gernet <skeimorr@gmail.com>
2023-10-09 18:39:43 +0400

    successful refactoring

--- js/main.js
+++ js/main.js
@@ -1,31 +1,19 @@
 import {getContractors} from './load-data.js';
 import {
   buySellContainer,
-  mapContainer,
   toggleListMapContainer,
 } from './variables.js';
 import {renderTable} from './render-table.js';
 import {onNavigationButtonClick, onToggleListMapContainerClick} from './navigation-controls.js';
-import {COPYRIGHT, startCoordinates, TILE_LAYER, ZOOM,} from './constants.js';
-import {createMarkers} from './create-markers.js';
 import {addModalWindowOpener} from './modal-window-opener.js';
-import {addCheckedUsersInputListener} from './checked-users-filter.js';
+import {initMap} from './map-init.js';
 
 let receivedData = [];
 getContractors().
   then((data) => {
     receivedData = data.slice();
     renderTable(data);
-    mapContainer.style.display = 'block'; //Необходимо для правильного отображения краты.
-    mapContainer.classList.add('visually-hidden');
-    const map = L.map('map').setView([startCoordinates.lat, startCoordinates.lng], ZOOM);
-    L.tileLayer(TILE_LAYER, {
-      attribution: COPYRIGHT
-    }).addTo(map);
-    const defaultMarkerGroup = L.layerGroup().addTo(map);
-    const verifiedMarkerGroup = L.layerGroup().addTo(map);
-    createMarkers(receivedData, defaultMarkerGroup, verifiedMarkerGroup);
-    addCheckedUsersInputListener(data, receivedData, map, defaultMarkerGroup);
+    initMap(data, receivedData);
     buySellContainer.addEventListener('click', onNavigationButtonClick);
     toggleListMapContainer.addEventListener('click', onToggleListMapContainerClick);
     addModalWindowOpener(receivedData);
---
+++ js/map-init.js
@@ -0,0 +1,20 @@
+import {mapContainer} from './variables.js';
+import {COPYRIGHT, startCoordinates, TILE_LAYER, ZOOM} from './constants.js';
+import {createMarkers} from './create-markers.js';
+import {addCheckedUsersInputListener} from './checked-users-filter.js';
+
+const initMap = (contractorsData, convertedData) => {
+  mapContainer.style.display = 'block'; //Необходимо для правильного отображения краты.
+  mapContainer.classList.add('visually-hidden');
+  const map = L.map('map').setView([startCoordinates.lat, startCoordinates.lng], ZOOM);
+  L.tileLayer(TILE_LAYER, {
+    attribution: COPYRIGHT
+  }).addTo(map);
+  const defaultMarkerGroup = L.layerGroup().addTo(map);
+  const verifiedMarkerGroup = L.layerGroup().addTo(map);
+  createMarkers(convertedData, defaultMarkerGroup, verifiedMarkerGroup);
+  addCheckedUsersInputListener(contractorsData, convertedData, map, defaultMarkerGroup);
+  return {convertedData};
+};
+
+export {initMap};
